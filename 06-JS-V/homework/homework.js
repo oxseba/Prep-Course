2ed3ab883aea30347601f60f103c154a81e057b0
Sebastian Acuña <sebastianacuna@192.168.1.6>
2022-10-27 21:42:05 -0300

    JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -63,18 +63,32 @@ function agregarStringInvertida() {
       this.apellido = apellido;
       this.edad = edad;
       this.domicilio = domicilio;  
-    }
-  } return Persona;
+      this.detalle = function() {
+        return {
+          Nombre: this.nombre,
+          Apellido: this.apellido,
+          Edad: this.edad,
+          Domicilio: this.domicilio,
+        }
+      }
+      }
+    } 
+
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  const persona = new Persona(nombre,apellido,edad,dir);
+  return persona;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function() {
+    return this.nombre + ", " + this.edad + " años";
+    }
 }
   
 
